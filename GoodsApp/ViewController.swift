319e091ddd2e96d2e7bcd766ce42f037593bbc12
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2022-11-09 19:48:40 +0300

    Initial Commit

---
+++ GoodsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodsApp
+//
+//  Created by jopootrivatel on 09.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
